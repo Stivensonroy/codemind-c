24eedab22ee1dc28bdefd209e37909659128d43a
Stivensonroy <119719954+Stivensonroy@users.noreply.github.com>
2022-12-19 22:56:26 +0530

    #include<stdio.h> int main(){     int n,c=0;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++){         scanf("%d ",&a[i]);     }     for(int i=0;i<n;i++){         if(a[i]==0){             printf("%d ",a[i]);         }     }     for(int i=0;i<n;i++){         if(a[i]==1){             printf("%d ",a[i]);         }     } }

---
+++ Sort_an_Array_of_0s_and_1s.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n,c=0;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d ",&a[i]);
+    }
+    for(int i=0;i<n;i++){
+        if(a[i]==0){
+            printf("%d ",a[i]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        if(a[i]==1){
+            printf("%d ",a[i]);
+        }
+    }
+}
