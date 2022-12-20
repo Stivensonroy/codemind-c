b067edd426c3c97d5f5134ba16c3b3d0889f08ab
Stivensonroy <119719954+Stivensonroy@users.noreply.github.com>
2022-12-20 21:55:40 +0530

    #include<stdio.h> int prime(int); int main(){     int n;     int count=0;     scanf("%d",&n);     for(int i=1;i<=n;i++){         if(n%i==0){             if(prime(i)){                 count++;             }         }     }     printf("%d",count);
    }
    int prime(int n){
        int c=0;
        for(int i=1;i<=n;i++){
            if(n%i==0){
                c++;
            }
        }
        if(c==2){
            return 0;
        }
        else{
            return 1;
        }
    }

---
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int prime(int);
+int main(){
+    int n;
+    int count=0;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            if(prime(i)){
+                count++;
+            }
+        }
+    }
+    printf("%d",count);
+    
+}
+int prime(int n){
+    int c=0;
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            c++;
+        }
+    }
+    if(c==2){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
