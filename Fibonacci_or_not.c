e1255ecca7db401b7c899cf86c71f460471485da
Stivensonroy <119719954+Stivensonroy@users.noreply.github.com>
2023-01-25 22:30:59 +0530

    #include<stdio.h> int main() {     int a,b,c,n;     scanf("%d",&n);     if(n==1||n==0)     {         printf("%d",n);     }     else     {         a=0;         b=1;         c=a+b;         while(c<n)         {             a=b;             b=c;             c=a+b;         }         if(c==n)         {             printf("True");         }         else         {             printf("False");         }     } }

---
+++ Fibonacci_or_not.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,n;
+    scanf("%d",&n);
+    if(n==1||n==0)
+    {
+        printf("%d",n);
+    }
+    else
+    {
+        a=0;
+        b=1;
+        c=a+b;
+        while(c<n)
+        {
+            a=b;
+            b=c;
+            c=a+b;
+        }
+        if(c==n)
+        {
+            printf("True");
+        }
+        else
+        {
+            printf("False");
+        }
+    }
+}
