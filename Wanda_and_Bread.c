dab50a23eff011cf17dee908de5c295b4f323ba3
Stivensonroy <119719954+Stivensonroy@users.noreply.github.com>
2023-01-26 00:39:29 +0530

    #include<stdio.h> int main() {     int x,y,z;     scanf("%d%d%d",&x,&y,&z);     if(y*z>=x)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Wanda_and_Bread.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    scanf("%d%d%d",&x,&y,&z);  
+    if(y*z>=x)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
